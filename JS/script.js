97269105a846c2f9a4b5c8c18c505962dcb33fe0
admin <shethinksnyc@protonmail.com>
2022-03-08 16:37:09 -0500

    nav bar responsiveness

--- JS/script.js
+++ JS/script.js
@@ -1,69 +1,58 @@
-const TypeWriter = function (txtElement, words, wait = 3000) {
-  // assign properties using this key word
-
-  // text element that is passed in
-  this.txtElement = txtElement;
-  // words that are passed in
-  this.words = words;
-  // what is in the area being typed
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
   this.txt = "";
-  // words are formatted as array- start at 0
-  this.wordIndex = 0;
-  // make sure wait is int , base param of 10
-  this.wait = parseInt(wait, 10);
-  // type method
-  this.type();
-  // set state
+  this.tick();
   this.isDeleting = false;
 };
 
-// Type Method
-TypeWriter.prototype.type = function () {
-  // get current Index of word
-  const current = this.wordIndex % this.words.length;
-  // get full text of current word
-  const fullTxt = this.words[current];
-  // check if in Deleting state
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
   if (this.isDeleting) {
-    // remove char
     this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-    // add char
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  // output every half sec
-  // insert txt into element
-  this.txt.innerHTML = `<span class="txt">${this.txt}</span>`;
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
-  // each time char is added or deleted
-  setTimeout(() => this.type(), 500);
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
 };
 
-// Init on DOM Load
-document.addEventListener("DOMContentLoaded", init);
-
-// Initialize App
-function init() {
-  // grab element
-  const txtElement = document.querySelector(".txt-type");
-  // grab words need to parse otherwise is string
-  const words = JSON.parse(txtElement.getAttribute("data-words"));
-  // grab wait
-  const wait = txtElement.getAttribute("data-wait");
-  // initialize typewriter
-
-  new TypeWriter(txtElement, words, wait);
-}
-
-// Navigation Bar
-
-// select button icon
-const navToggle = document.querySelector(".navbar_toggle");
-
-//select ul class
-const navLinks = document.querySelector(".main__nav");
-
-navToggle.addEventListener("click", function () {
-  navLinks.classList.toggle("show_nav");
-});
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
